c1684ddea6815c5c965477a194e856f7dbab81e6
Dice1111 <kingdice1111@gmail.com>
2022-04-03 14:59:10 +0630

    hook

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="home">
-    <input type="text" v-model="search">
-    <p>Search item={{search}}</p>
-    <div v-for="name in filteredNames" :key="name">
-      {{name}}
+    <div v-if="showPosts">
+      <PostsList :posts="posts"></PostsList>
     </div>
+    <button @click="showPosts=!showPosts">toggle post lists</button>
+    <button @click="posts.pop()">delete post lists</button>
+
   </div>
 </template>
 
 <script>
+import PostsList from '../components/PostsList'
 import {  computed, ref } from '@vue/runtime-core'
 
 
 
 export default {
+  components: { PostsList },
   setup(){
-    let search=ref ("");
-    let names = ref(["aung","mg","su"]);
-    let filteredNames=computed(()=>{
-      return names.value.filter(name=>{
-        return name.includes(search.value)
-      })
-    })
-    return{names,search,filteredNames};
+    let showPosts=ref(true);
+    let posts=ref([
+      {title:"post title 1",body:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, sequi dolor aperiam minima saepe dolores exercitationem reiciendis id consectetur impedit, tenetur vero ratione atque voluptate placeat voluptatibus eaque, similique illum.",id:1},
+      {title:"post title 2",body:"Lorem ipusm",id:2},
+    ])
+    return {showPosts,posts};
   }
 }
 </script>
