0bcabee0d001010950b223233d127f57b22158ba
Dice1111 <kingdice1111@gmail.com>
2022-04-03 15:43:49 +0630

    fetching posts data with aync and await function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
-
+    <div v-if="error">
+      {{error}}
+    </div>
+    <div v-if="posts.length>0">
       <PostsList :posts="posts"></PostsList>
+    </div>
+    <div v-else>Loading</div>  
 
 
 
@@ -11,6 +16,7 @@
 <script>
 import PostsList from '../components/PostsList'
 import {  computed, ref } from '@vue/runtime-core'
+import PostsListVue from '@/components/PostsList.vue';
 
 
 
@@ -18,9 +24,25 @@ export default {
   components: { PostsList },
   setup(){
 
-    let posts=ref([      
-    ])
-    return {posts};
+    let posts=ref([]);
+    let error=ref("");
+
+    let load=async()=>{
+      try{
+        let response= await fetch("http://localhost:3000/posts")
+        if(response.status===404){
+          throw new Error("not found url");
+        }
+        let datas=await response.json();
+        posts.value=datas;
+      }catch(err){
+        error.value=err.message;
+      }
+    }
+
+    load();
+
+    return {posts,error};
   }
 }
 </script>
