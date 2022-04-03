acdc5fc0a1cb10cc749fb5f879e81a3f57249937
Dice1111 <kingdice1111@gmail.com>
2022-04-03 15:09:05 +0630

    project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="home">
-    <div v-if="showPosts">
+
       <PostsList :posts="posts"></PostsList>
-    </div>
-    <button @click="showPosts=!showPosts">toggle post lists</button>
-    <button @click="posts.pop()">delete post lists</button>
+
+
 
   </div>
 </template>
@@ -18,12 +17,10 @@ import {  computed, ref } from '@vue/runtime-core'
 export default {
   components: { PostsList },
   setup(){
-    let showPosts=ref(true);
-    let posts=ref([
-      {title:"post title 1",body:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, sequi dolor aperiam minima saepe dolores exercitationem reiciendis id consectetur impedit, tenetur vero ratione atque voluptate placeat voluptatibus eaque, similique illum.",id:1},
-      {title:"post title 2",body:"Lorem ipusm",id:2},
+
+    let posts=ref([      
     ])
-    return {showPosts,posts};
+    return {posts};
   }
 }
 </script>
